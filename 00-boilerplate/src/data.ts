25d989adc7d5bb677d9937b081951c9221cf370a
Braulio Diez Botella <braulio.diez@gmail.com>
2020-02-20 21:52:25 +0100

    added results data

---
+++ 00-boilerplate/src/data.ts
@@ -0,0 +1,63 @@
+interface resultEntry {
+  party: string;
+  seats: number;
+}
+
+export const resultCollectionSpainNov19: resultEntry[] = [
+  {
+    party: "PSOE",
+    seats: 120
+  },
+  {
+    party: "PP",
+    seats: 88
+  },
+  {
+    party: "VOX",
+    seats: 52
+  },
+  {
+    party: "UP",
+    seats: 35
+  },
+  {
+    party: "ERC",
+    seats: 13
+  },
+  {
+    party: "Cs",
+    seats: 10
+  },
+  {
+    party: "JxCat",
+    seats: 8
+  },
+  {
+    party: "PNV",
+    seats: 7
+  },
+  {
+    party: "Bildu",
+    seats: 5
+  },
+  {
+    party: "Más pais",
+    seats: 4
+  },
+  {
+    party: "CUP",
+    seats: 2
+  },
+  {
+    party: "CC",
+    seats: 2
+  },
+  {
+    party: "BNG",
+    seats: 1
+  },
+  {
+    party: "Teruel Existe",
+    seats: 1
+  }
+];
