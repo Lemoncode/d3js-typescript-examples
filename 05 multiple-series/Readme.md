953f9eea164f6f82ce827ac7d338080cce8d748c
Braulio Diez Botella <braulio.diez@gmail.com>
2020-02-27 22:00:22 +0100

    basic animation

---
+++ 05 multiple-series/Readme.md
@@ -0,0 +1,78 @@
+# Pimp Chart
+
+"You nailed it!" the boss said, that's great, buuuuut... he asked as well if
+we can just pimp a bit the chart, it is going to be published on an online
+newspaper and it needs some details like add a legend to the chart...
+
+Something like:
+
+![pimped chart](./content/chart.png "pimped chart")
+
+Live demo: [codesandbox]()
+
+# Steps
+
+- We will take as starting sample _02B-single-bar-stack-layout_.
+
+- Let's copy the content from _02B-single-bar-stack-layout_ and execute _npm install_
+
+```bash
+npm install
+```
+
+- Let's install _d3-svg-legend_
+
+```bash
+npm i d3-svg-legend -S
+```
+
+-
+
+```diff
+import * as d3 from "d3";
+import { resultCollectionSpainNov19 } from "./data";
++ import {legendColor} from 'd3-svg-legend'
+```
+
+- Now let's create and ordinal scale color,map it to a legend object and add it in a group below the semi arch chart.
+
+```typescript
+// Legend
+var ordinal = d3
+  .scaleOrdinal()
+  .domain(politicalPartiesKeys)
+  .range(partiesColor);
+
+var legendOrdinal = legendColor().scale(ordinal);
+
+const legendLeft = margin.left;
+const legendTop = radius + 5;
+
+const legendGroup = svg
+  .append("g")
+  .attr("transform", `translate(${legendLeft},${legendTop})`);
+
+legendGroup.call(legendOrdinal);
+```
+
+# Excercise
+
+A) We have shown a legend where all elements are in single columns, what if we want to split them in two columns?
+
+- We could use two legends objects and split the colors / domain.
+- We could play creating our custom legend.
+
+Tips: https://stackoverflow.com/questions/51520596/spread-d3-js-legend-on-two-columns/51524137
+Tips: http://jsfiddle.net/v7mkg/1/
+
+B) Play a bit... add a rectangle, interact when clicking on a given arc (e.g. display tooltip)...
+
+# About Basefactor + Lemoncode
+
+We are an innovating team of Javascript experts, passionate about turning your ideas into robust products.
+
+[Basefactor, consultancy by Lemoncode](http://www.basefactor.com) provides consultancy and coaching services.
+
+[Lemoncode](http://lemoncode.net/services/en/#en-home) provides training services.
+
+For the LATAM/Spanish audience we are running an Online Front End Master degree, more info: http://lemoncode.net/master-frontend
---
+++ 05 multiple-series/src/index.ts
@@ -0,0 +1,146 @@
+import * as d3 from "d3";
+import {
+  resultCollectionSpainNov19,
+  resultCollectionSpainApr19,
+  ResultEntry
+} from "./data";
+import { legendColor } from "d3-svg-legend";
+import { entries } from "d3";
+
+const svgDimensions = { width: 800, height: 500 };
+const margin = { left: 5, right: 5, top: 10, bottom: 10 };
+const chartDimensions = {
+  width: svgDimensions.width - margin.left - margin.right,
+  height: svgDimensions.height - margin.bottom - margin.top
+};
+const totalNumberSeats = resultCollectionSpainNov19.reduce(
+  (sum, item) => sum + item.seats,
+  0
+);
+const politicalPartiesCount = resultCollectionSpainNov19.length;
+const barHeight = 200;
+
+const politicalPartiesKeys: string[] = [
+  "PSOE",
+  "PP",
+  "VOX",
+  "UP",
+  "ERC",
+  "Cs",
+  "JxCat",
+  "PNV",
+  "Bildu",
+  "Más pais",
+  "CUP",
+  "CC",
+  "BNG",
+  "Teruel Existe",
+  "Compromis"
+];
+
+const partiesColor = [
+  "#ED1D25",
+  "#0056A8",
+  "#5BC035",
+  "#6B2E68",
+  "#F3B219",
+  "#FA5000",
+  "#C50048",
+  "#029626",
+  "#A3C940",
+  "#0DDEC5",
+  "#FFF203",
+  "#FFDB1B",
+  "#E61C13",
+  "#73B1E6",
+  "#BECD48",
+  "#017252",
+  "#DD0000"
+];
+
+var ordinal = d3
+  .scaleOrdinal()
+  .domain(politicalPartiesKeys)
+  .range(partiesColor);
+
+const svg = d3
+  .select("body")
+  .append("svg")
+  .attr("width", svgDimensions.width)
+  .attr("height", svgDimensions.height)
+  .attr("style", "background-color: #FBFAF0");
+
+const chartGroup = svg
+  .append("g")
+  .attr("transform", `translate(${margin.left}, ${margin.top})`)
+  .attr("width", chartDimensions.width)
+  .attr("height", chartDimensions.height);
+
+const radius = Math.min(chartDimensions.width, chartDimensions.height) / 2;
+
+chartGroup.attr("transform", `translate(${radius},${radius})`);
+
+const arc = d3
+  .arc()
+  .innerRadius(radius / 1.7) // We want to have an arc with a propotional width
+  .outerRadius(radius);
+
+const pieChart = d3
+  .pie()
+  .startAngle(-90 * (Math.PI / 180))
+  .endAngle(90 * (Math.PI / 180))
+  .value(d => d["seats"])
+  .sort(null);
+
+const pie = pieChart(<any>resultCollectionSpainNov19);
+
+const arcs = chartGroup
+  .selectAll("slice")
+  .data(pie)
+  .enter();
+
+arcs
+  .append("path")
+  .attr("d", <any>arc) // Hack typing: https://stackoverflow.com/questions/35413072/compilation-errors-when-drawing-a-piechart-using-d3-js-typescript-and-angular/38021825
+  .attr("fill", (d, i) => partiesColor[i]); // TODO color ordinal
+
+// Legend
+
+var legendOrdinal = legendColor().scale(ordinal);
+
+const legendLeft = margin.left;
+const legendTop = radius + 5;
+
+const legendGroup = svg
+  .append("g")
+  .attr("transform", `translate(${legendLeft},${legendTop})`);
+
+legendGroup.call(legendOrdinal);
+
+document
+  .getElementById("april")
+  .addEventListener("click", function handleResultsApril() {
+    updateChart(resultCollectionSpainApr19);
+  });
+
+document
+  .getElementById("november")
+  .addEventListener("click", function handleResultsNovember() {
+    updateChart(resultCollectionSpainNov19);
+  });
+
+// Smoother Transition: https://bl.ocks.org/tezzutezzu/c2653d42ffb4ecc01ffe2d6c97b2ee5e
+const updateChart = (data: ResultEntry[]) => {
+  let path = svg.selectAll("path");
+  const data0 = path.data();
+  const data1 = pieChart(<any>data);
+
+  path = path.data(data1);
+
+  d3.selectAll("path")
+    .data(pieChart(<any>data))
+    .transition()
+    .duration(1000)
+    .attr("d", <any>arc);
+
+};
